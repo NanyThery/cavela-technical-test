0c7bc6ad210e5b2a7215c3d820522d5e9d652ac1
NanyThery <nadinethery@gmail.com>
2024-02-20 19:30:54 +0100

    add remove function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,50 @@
+"use client";
+import { useEffect, useState } from "react";
 import CardGrid from "../components/CardGrid/CardGrid";
 import SectionTitle from "../components/UI/SectionTitle";
 import fetchQuotes from "./actions";
 import CardQuote from "@/components/CardQuote/CardQuote";
+import { QuotationCard } from "../types/QuotationCard.interface";
+import CardGridSkeletons from "../components/Skeletons/CardGridSkeletons";
 
-export default async function HomePage() {
-  const quotes = await fetchQuotes();
+export default function HomePage() {
+  const [quotes, setQuotes] = useState<QuotationCard[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  // This is done because we are not tackling a real ddbb. If we had a real data base, we wouldn't need to use  'use client' or useEffect, since we could do the actions server side and ask the ddbb for the updated array of data after deletion.
+
+  useEffect(() => {
+    async function fetchQuotesFun() {
+      const quotes = await fetchQuotes();
+      setLoading(false);
+      setQuotes(quotes);
+    }
+
+    fetchQuotesFun();
+  }, []);
+
+  function handleRemove(index: number) {
+    const updatedQuotes = [...quotes];
+    updatedQuotes.splice(index, 1);
+    setQuotes(updatedQuotes);
+  }
+
   return (
     <div>
       <SectionTitle title="Quote Selection" />
-      <CardGrid>
-        {quotes.map((q, index) => (
-          <CardQuote key={index} quote={q} />
-        ))}
-      </CardGrid>
+      {loading ? (
+        <CardGridSkeletons />
+      ) : (
+        <CardGrid>
+          {quotes.map((q, index) => (
+            <CardQuote
+              key={index}
+              quote={q}
+              onRemove={() => handleRemove(index)}
+            />
+          ))}
+        </CardGrid>
+      )}
     </div>
   );
 }
--- src/components/CardQuote/CardQuote.tsx
+++ src/components/CardQuote/CardQuote.tsx
@@ -1,14 +1,23 @@
+"use client";
 import { QuotationCard } from "../../types/QuotationCard.interface";
 import Tag from "../UI/Tag";
 import styles from "./CardQuote.module.css";
 import RatingTag from "../RatingTag/RatingTag";
 import CardTable from "../CardTable/CardTable";
+import { RemoveButton } from "../RemoveButton/RemoveButton";
+import { ColorVariant } from "../../types/ColorVariant.type";
+import { selectTagVariant } from "../../utils/selectTagVariant";
 
 interface CardQuoteProps {
   quote: QuotationCard;
+  onRemove: () => void;
 }
 
-export default function CardQuote({ quote }: CardQuoteProps) {
+export default function CardQuote({ quote, onRemove }: CardQuoteProps) {
+  function handleRemove() {
+    onRemove();
+  }
+
   return (
     <div className={styles.cardWrapper}>
       <span className={styles.cavelasChoice}>
@@ -16,12 +25,18 @@ export default function CardQuote({ quote }: CardQuoteProps) {
       </span>
 
       <div className={styles.cardContainer}>
+        <div className={styles.cardRemoveContainer}>
+          <RemoveButton onClick={handleRemove} />
+        </div>
         <div className={styles.cardHeader}>
           <div className={styles.cardHeaderLeft}>
             <h2>{quote.supplier.name}</h2>
             <div className={styles.tagContainer}>
               {quote.badges.map((badge, index) => (
-                <Tag key={index}>{`#${badge[1]} ${badge[0]}`}</Tag>
+                <Tag
+                  key={index}
+                  variant={selectTagVariant(index)}
+                >{`#${badge[1]} ${badge[0]}`}</Tag>
               ))}
             </div>
           </div>
